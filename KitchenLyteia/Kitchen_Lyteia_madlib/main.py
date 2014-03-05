6eebae29d0a0bb2339c7bd58cb4042b55c4e1425
Lyteia Kitchen <lytehighfashion@gmail.com>
2014-03-04 21:54:34 -0500

    Set up of  dictionary

---
+++ KitchenLyteia/Kitchen_Lyteia_madlib/main.py
@@ -0,0 +1,15 @@
+'''
+Lyteia Kitchen
+3/4/2014
+Lab 1
+'''
+#Dictionary Set Up
+data = dict ()
+
+data['name'] = input("Whats your name")
+data['school'] = input("What School You Go To")
+data['grade_level'] = input("What Grade Are You In")
+data['time'] = input("How Many Years You Been Here")
+dat['grades'] = []
+
+
