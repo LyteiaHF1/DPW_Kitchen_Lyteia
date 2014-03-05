6d3583cd1ecd39d0b09622097a77246be2d7a97b
Lyteia Kitchen <lytehighfashion@gmail.com>
2014-03-04 21:55:15 -0500

    fixed spelling error

--- KitchenLyteia/Kitchen_Lyteia_madlib/main.py
+++ KitchenLyteia/Kitchen_Lyteia_madlib/main.py
@@ -10,6 +10,6 @@ data['name'] = input("Whats your name")
 data['school'] = input("What School You Go To")
 data['grade_level'] = input("What Grade Are You In")
 data['time'] = input("How Many Years You Been Here")
-dat['grades'] = []
+data['grades'] = []
 
 
