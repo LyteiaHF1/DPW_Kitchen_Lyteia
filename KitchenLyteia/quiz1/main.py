e1b789407dd73c4795c2c792f7e911a608a01925
Lyteia Kitchen <lytehighfashion@gmail.com>
2014-03-11 22:10:56 -0400

    End of commenting code

--- KitchenLyteia/quiz1/main.py
+++ KitchenLyteia/quiz1/main.py
@@ -12,10 +12,11 @@ class CalcArea():
 def calcArea(w, h):
     #area = h times width
     area = h * w
+    #this returns the area
     return area
 
     area = calcArea(height, width)
-
+    #if height is the same a width
     if height == width:
         print "The area of your square is" + str(area)+ "square feet."
     else:
@@ -29,6 +30,8 @@ def calcArea(w, h):
 class Beer():
     bottle  = 100
     print str(bottle) + "bottles of beer on the wall"+ str(bottle) + "bottles of beer... you take one down pass it around " + str(bottle -1) + "bottles of beer on the wall"
+    #bottle greater than 1
     while bottle > 1:
+        #bottle = -1 (take one away each time)
         bottle -=1
         print str(bottle) + "bottles of beer on the wall"+ str(bottle) + "bottles of beer... you take one down pass it around " + str(bottle -1) + "bottles of beer on the wall"
