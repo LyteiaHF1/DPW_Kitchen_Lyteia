4ce5e368c718fc06edf4bd8b28d3da2e359150c4
Lyteia Kitchen <lytehighfashion@gmail.com>
2014-03-13 15:48:07 -0400

    start of shoe instances

--- KitchenLyteia/calc/main.py
+++ KitchenLyteia/calc/main.py
@@ -13,6 +13,23 @@ class MainHandler(webapp2.RequestHandler):
         #import page
         page = Page()
 
+        #airJordan Shoe Instance
+
+        airj = Shoe()
+        airj.name = 'Air Jordans'
+        airj.price = 180 #$
+        airj.limit = 2 #pairs
+        airj.img ="http://upload.wikimedia.org/wikipedia/en/3/37/Jumpman_logo.svg"
+
+        #nikeFoampositePro Shoe Instance
+
+        nfp = Shoe()
+        nfp.name = 'Nike Foamposite Pro'
+        nfp.price = 220 #$
+        nfp.limit = 1 #pairs
+        nfp.img = "http://upload.wikimedia.org/wikipedia/en/3/37/Jumpman_logo.svg"
+
+
  #Objects
 class Shoe(object):
     def __init__(self):
