1d5b203831856da07a0bf86bcaddeae92994d493
Lyteia Kitchen <lytehighfashion@gmail.com>
2014-03-13 15:43:54 -0400

    adding

--- KitchenLyteia/calc/page.py
+++ KitchenLyteia/calc/page.py
@@ -5,4 +5,17 @@ Lab 3
 '''
 class Page():
     def __init__(self):
-        pass
+        self.__header = '''
+<!DOCTYPE>
+<html>
+    <head>
+        <title>Top 5 Sneakers</title>
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <div id="wrapper">
+            <header>
+                <h1>Top 5 Sneakers </h1>
+            </header>
+            <div id="main">
+        '''
