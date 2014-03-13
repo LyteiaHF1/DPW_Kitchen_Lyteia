bbc5c8fef5c1e48d5be9520adcef64978b1a21ad
Lyteia Kitchen <lytehighfashion@gmail.com>
2014-03-13 15:47:12 -0400

    shoe class object

--- KitchenLyteia/calc/main.py
+++ KitchenLyteia/calc/main.py
@@ -13,6 +13,16 @@ class MainHandler(webapp2.RequestHandler):
         #import page
         page = Page()
 
+ #Objects
+class Shoe(object):
+    def __init__(self):
+        self.name = ''
+        self.__price = 0
+        self.__limit = 0
+        self.img = ''
+
+
+
 
 
 app = webapp2.WSGIApplication([
