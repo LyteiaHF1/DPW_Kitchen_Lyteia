5df3b7f5fe2aa34d7b8e27ebea0ac911ba072478
Lyteia Kitchen <lytehighfashion@gmail.com>
2014-03-13 21:54:14 -0400

    Should be Sneaker not Sneakers ! start of jordan class

--- KitchenLyteia/quiz2/main.py
+++ KitchenLyteia/quiz2/main.py
@@ -3,10 +3,15 @@ Lyteia
 Quiz 2
 3/13/14
 '''
-#sneakers class 
-class Sneakers(object):
+#sneakers class
+class Sneaker(object):
     def __init__(self):
         self.__type  = "Retro or Heat"
         self.__sport = "Basketball or Running"
         self.__brand = "Jordan,Lebron,Kobe"
 
+
+
+class Jordan(Sneaker):
+    def __init__(self):
+        self.sport = "Basketball"
