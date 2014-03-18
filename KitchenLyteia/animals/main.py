b5eade55968199fadae71b9dad08a5c72c54cb30
Lyteia Kitchen <lytehighfashion@gmail.com>
2014-03-18 12:01:13 -0400

    start of lab 4

---
+++ KitchenLyteia/animals/main.py
@@ -0,0 +1,12 @@
+'''
+Lyteia
+Lab4
+3/13/14
+'''
+import webapp2
+from page import Page
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        pass
+
---
+++ KitchenLyteia/animals/page.py
@@ -0,0 +1,11 @@
+'''
+Lyteia
+Lab 4
+3/13/14
+'''
+
+
+class Page():
+    def __init__(self):
+
+        pass
