f6cba612a0a6e278daac171b604a10860707a49f
Lyteia Kitchen <lytehighfashion@gmail.com>
2014-03-18 12:03:09 -0400

    HTML Set Up In page.py

--- KitchenLyteia/animals/page.py
+++ KitchenLyteia/animals/page.py
@@ -8,4 +8,20 @@ Lab 4
 class Page():
     def __init__(self):
 
-        pass
+        self.__header = '''
+<!DOCTYPE>
+<html>
+<head>
+<title>Countries</title>
+<link rel='stylesheet' href='css/main.css' />
+</head>
+<body>
+<div id="container">'''
+        self.__form='''
+<nav>
+<a href='/?animal=0'><button></button></a>
+<a href='/?animal=1'><button></button></a>
+<a href='/?animal=2'><button></button></a>
+<a href='/?animal=3'><button></button></a>
+<a href='/?animal=4'><button></button></a>
+</nav>'''
