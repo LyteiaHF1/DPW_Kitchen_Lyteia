1e6be517af6872d77dd5908f8865646bd03ae9ac
Lyteia Kitchen <lytehighfashion@gmail.com>
2014-03-20 21:18:19 -0400

    Start of sneaker class

---
+++ KitchenLyteia/quiz3/main.py
@@ -0,0 +1,12 @@
+'''
+Lyteia
+Quiz 3
+3/20/14
+'''
+class Sneaker(object):
+    def __init__(self):
+        self.__ryear = '2012'
+        self._type = 'Jordan'
+        self.__color = 'Black'
+
+        
