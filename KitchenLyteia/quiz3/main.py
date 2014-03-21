aa8cb5c897a9945f98ea88cb7e69f854a0474d18
Lyteia Kitchen <lytehighfashion@gmail.com>
2014-03-20 21:20:29 -0400

    Jordan class

--- KitchenLyteia/quiz3/main.py
+++ KitchenLyteia/quiz3/main.py
@@ -9,4 +9,15 @@ class Sneaker(object):
         self._type = 'Jordan'
         self.__color = 'Black'
 
-        
+
+
+class Jordan(Sneaker):
+    def __init__(self):
+        super(Jordan, self).__init__()
+
+        self.__ryear = '1994'
+        self._type = 'Jordan'
+        self.__style = 'Retro 11'
+        self.condition = 'Very Near Deadstock'
+
+       
