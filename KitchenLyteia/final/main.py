6cdea3401fcd045c5546da2b8aa39127558f19e3
Lyteia Kitchen <lytehighfashion@gmail.com>
2014-03-27 19:09:22 -0400

    Basic setup

--- KitchenLyteia/final/main.py
+++ KitchenLyteia/final/main.py
@@ -4,10 +4,17 @@ DPW Final
 March 27,2014
 '''
 import webapp2
-
+import urllib2#imoprt for xml
+from xml.dom import minidom#import for minidom
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        pass
+
+
+
+
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
--- KitchenLyteia/final/page.py
+++ KitchenLyteia/final/page.py
@@ -3,3 +3,7 @@ Lyteia
 DPW Final
 March 27,2014
 '''
+import urllib2#imoprt for xml
+from xml.dom import minidom#import for minidom
+class Model(object):
+    pass
